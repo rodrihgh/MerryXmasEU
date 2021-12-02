674fe3df1ab9405105065220caaffbe8316a0eee
Rodrigo Hernangomez <rodrigoherher@gmail.com>
2021-12-02 05:00:11 +0100

    Adding day counting logic to the tweet

--- tweet.py
+++ tweet.py
@@ -5,6 +5,7 @@ Minimal script to post a tweet to the Twitter API using a given message.
 """
 import os
 import sys
+from datetime import datetime as dt, timedelta as td
 
 import tweepy
 
@@ -13,6 +14,42 @@ CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_KEY = os.environ.get('ACCESS_KEY')
 ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+
+header = {"ES": "Querida _CE_, hoy es"}
+
+weekdays = {"ES": ("lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo")}
+
+advent_week = {"ES": lambda d: f"de la {d}ª semana de Adviento."}
+
+remaining = {"ES": lambda d: f"Faltan {d} días para desearos una muy _FelizNavidad"}
+
+today = dt.date(dt.now())
+year = today.year
+if today.month < 3:
+    year -= 1
+
+christmas = dt.date(dt(year=year, month=12, day=25))
+xmas_weekday = christmas.weekday()
+
+epiphany = dt.date(dt(year=year+1, month=1, day=6))
+baptism = epiphany # TODO calculate next Sunday
+candlemas = dt.date(dt(year=year+1, month=2, day=2))
+
+fourth_advent = christmas - td(days=(xmas_weekday+1))
+third_advent = fourth_advent - td(days=7)
+second_advent = third_advent - td(days=7)
+first_advent = second_advent - td(days=7)
+
+celebrations = {first_advent: {"ES": "1er domingo de Adviento."},
+                second_advent: {"ES": "2º domingo de Adviento."},
+                third_advent: {"ES": "3er domingo de Adviento."},
+                fourth_advent: {"ES": "4º domingo de Adviento."},
+                dt.date(dt(year=year, month=12, day=24)): {"ES": "Nochebuena."},
+                christmas: {"ES": "Navidad"},
+                epiphany: {"ES": "Epifanía, día de los Reyes Magos"},
+                baptism: {"ES": "domingo, día del Bautizo del Señor"},
+                candlemas: {"ES": "Fiesta de la Candelaria"}}
+celeb_days = celebrations.keys()
 
 
 def setup_conn():
@@ -36,6 +73,22 @@ def get_client():
     return tweepy.Client(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_KEY, access_token_secret=ACCESS_SECRET)
 
 
+def write_tweet(lang="ES"):
+    if first_advent <= today < christmas:
+        if today in celeb_days:
+            day_text = celebrations[today][lang]
+        else:
+            weekday = weekdays[lang][today.weekday()]
+            adv_week = (today - first_advent).days // 7 + 1
+            day_text = " ".join([weekday, advent_week[lang](adv_week)])
+        rem_days = (christmas - today).days
+        return " ".join([header[lang], day_text, remaining[lang](rem_days)])
+    elif christmas <= today <= candlemas:
+        pass  # TODO
+    else:
+        print("Christmas Season is over :(")
+
+
 def main(args):
     """
     Command-line entrypoint to post a tweet message to Twitter.
@@ -46,9 +99,7 @@ def main(args):
 
         sys.exit(1)
 
-    msg = args[0]
-    if not msg:
-        print("Message must not be an empty value")
+    msg = write_tweet()
 
     # api = setup_conn()
     client = get_client()
